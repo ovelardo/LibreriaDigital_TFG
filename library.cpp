62abffdf3bf10c0d1742c10b6eef6b9bb96d04ff
ovelardo <ovelardo@gmail.com>
2023-06-14 12:50:42 +0200

    Prueba de rama en perfilado

--- library.cpp
+++ library.cpp
@@ -247,6 +247,7 @@ void edgeDetection(unsigned short* src, unsigned short* dst, int rows, int cols,
 
             // Mapeamos los valores al rango 0-65535
             result = (result + 131070) * (65535 / 458740);
+
             dst[i * cols + j] = static_cast<unsigned short>(result);
         }
     }
